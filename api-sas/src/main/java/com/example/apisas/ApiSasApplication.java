8c3ac5325c779b19d91307816f9b1b6c2f4893b1
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-04-20 12:26:38 +0700

    add front/back project

---
+++ api-sas/src/main/java/com/example/apisas/ApiSasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSasApplication.class, args);
+    }
+
+}
