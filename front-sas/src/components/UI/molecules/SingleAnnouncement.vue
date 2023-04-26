bc9d53869b7949eb43083a00722c1e9c8830d289
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-04-26 22:10:29 +0700

    Update SingleAnnouncement.vue

--- front-sas/src/components/UI/molecules/SingleAnnouncement.vue
+++ front-sas/src/components/UI/molecules/SingleAnnouncement.vue
@@ -1,13 +1,42 @@
 <script setup>
 import BadgeCategories from './BadgeCategories.vue';
 import DetailIcon from '../atoms/DetailIcon.vue';
+import { onMounted, reactive, ref, watchEffect } from 'vue';
 const props = defineProps({
   announcementItem: {
     type: Object,
     required: true,
   },
 });
-
+const badgeCategoryColor = reactive({
+  textColor: '',
+  bgColor: '',
+});
+watchEffect(() => {
+  const categoryId = props.announcementItem.category.categoryId;
+  switch (categoryId) {
+    case 1:
+      badgeCategoryColor.bgColor = 'bg-green-300';
+      badgeCategoryColor.textColor = 'text-[#145941]';
+      // console.log('ทั่วไป');
+      break;
+    case 2:
+      badgeCategoryColor.bgColor = 'bg-[#55CAF5]';
+      badgeCategoryColor.textColor = 'text-[#244F5F]';
+      // console.log('ทุนการศึกษา');
+      break;
+    case 3:
+      badgeCategoryColor.bgColor = 'bg-[#FF9DA8]';
+      badgeCategoryColor.textColor = 'text-[#8D212D]';
+      // console.log('หางาน');
+      break;
+    case 4:
+      badgeCategoryColor.bgColor = 'bg-yellow-300';
+      badgeCategoryColor.textColor = 'text-yellow-800';
+      // console.log('ฝึกงาน');
+      break;
+  }
+});
 const onClickDetail = () => {
   console.log(props.announcementItem.id);
 };
@@ -26,9 +55,11 @@ const onClickDetail = () => {
       {{ announcementItem.announcementTitle }}
     </div>
     <div class="w-1/3 text-center">
-      <BadgeCategories bg-color="bg-green-300" text-color="text-[#145941]">{{
-        announcementItem.category.announcementCategory
-      }}</BadgeCategories>
+      <BadgeCategories
+        :bg-color="badgeCategoryColor.bgColor"
+        :text-color="badgeCategoryColor.textColor"
+        >{{ announcementItem.category.announcementCategory }}</BadgeCategories
+      >
     </div>
 
     <div class="text-xl text-center w-2/3">
