0b19a064764e80be01c1de08087ca7b7b4e07d11
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-04-26 14:41:13 +0700

    Update NavigationBar.vue

--- front-sas/src/components/UI/organisms/NavigationBar.vue
+++ front-sas/src/components/UI/organisms/NavigationBar.vue
@@ -1,7 +1,40 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <nav class="bg-pink-400 border-gray-200">
+    <div
+      class="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4"
+    >
+      <!-- Add Logo & Name -->
+      <a href="" class="flex items-center">
+        <img src="" class="h-8 mr-3" alt="Geegie2P Logo" />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >Geegie2P</span
+        >
+      </a>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 pl-3 pr-4 text-white text-xl hover:text-gray-300 rounded"
+              >Home</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 pl-3 pr-4 text-white text-xl hover:text-gray-300 rounded"
+              >About</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped></style>
