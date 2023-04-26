35c188d7c85d3ec656db66015522518fdc996c3a
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-04-26 14:28:20 +0700

    Add Component
    - Create New Atomic Design
    - Add Hompage
    - Add PageTemplate
    - Add NavigationBar

---
+++ front-sas/src/components/pages/Homepage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import PageTemplate from '../templates/PageTemplate.vue';
+import NavigationBar from '../UI/organisms/NavigationBar.vue';
+</script>
+
+<template>
+  <PageTemplate>
+    <NavigationBar />
+  </PageTemplate>
+</template>
+
+<style scoped></style>
