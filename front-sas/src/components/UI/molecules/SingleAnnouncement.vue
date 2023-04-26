7961c7c896664b09708ed4c38e0b728f7e1395a9
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-04-26 22:39:31 +0700

    Add Modal

--- front-sas/src/components/UI/molecules/SingleAnnouncement.vue
+++ front-sas/src/components/UI/molecules/SingleAnnouncement.vue
@@ -1,7 +1,7 @@
 <script setup>
-import BadgeCategories from './BadgeCategories.vue';
-import DetailIcon from '../atoms/DetailIcon.vue';
-import { onMounted, reactive, ref, watchEffect } from 'vue';
+import BadgeCategories from "./BadgeCategories.vue";
+import DetailIcon from "../atoms/DetailIcon.vue";
+import { onMounted, reactive, ref, watchEffect } from "vue";
 const props = defineProps({
   announcementItem: {
     type: Object,
@@ -9,36 +9,43 @@ const props = defineProps({
   },
 });
 const badgeCategoryColor = reactive({
-  textColor: '',
-  bgColor: '',
+  textColor: "",
+  bgColor: "",
 });
 watchEffect(() => {
   const categoryId = props.announcementItem.category.categoryId;
   switch (categoryId) {
     case 1:
-      badgeCategoryColor.bgColor = 'bg-green-300';
-      badgeCategoryColor.textColor = 'text-[#145941]';
+      badgeCategoryColor.bgColor = "bg-green-300";
+      badgeCategoryColor.textColor = "text-[#145941]";
       // console.log('ทั่วไป');
       break;
     case 2:
-      badgeCategoryColor.bgColor = 'bg-[#55CAF5]';
-      badgeCategoryColor.textColor = 'text-[#244F5F]';
+      badgeCategoryColor.bgColor = "bg-[#55CAF5]";
+      badgeCategoryColor.textColor = "text-[#244F5F]";
       // console.log('ทุนการศึกษา');
       break;
     case 3:
-      badgeCategoryColor.bgColor = 'bg-[#FF9DA8]';
-      badgeCategoryColor.textColor = 'text-[#8D212D]';
+      badgeCategoryColor.bgColor = "bg-[#FF9DA8]";
+      badgeCategoryColor.textColor = "text-[#8D212D]";
       // console.log('หางาน');
       break;
     case 4:
-      badgeCategoryColor.bgColor = 'bg-yellow-300';
-      badgeCategoryColor.textColor = 'text-yellow-800';
+      badgeCategoryColor.bgColor = "bg-yellow-300";
+      badgeCategoryColor.textColor = "text-yellow-800";
       // console.log('ฝึกงาน');
       break;
   }
 });
+const emits = defineEmits(["announcementDetail", "showModal"]);
+const showModal = ref(false);
+const selectedAnnouncement = ref();
 const onClickDetail = () => {
+  showModal.value = true;
   console.log(props.announcementItem.id);
+  selectedAnnouncement.value = props.announcementItem.id;
+  emits("announcementDetail", selectedAnnouncement.value);
+  emits("showModal", showModal.value);
 };
 </script>
 
---
+++ front-sas/src/components/UI/organisms/AnnouncementDetail.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ModalTemplate from "../../templates/ModalTemplate.vue";
+const props = defineProps({
+  announcementDetail: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <Teleport to="body">
+    <ModalTemplate :showModal="isOpen"> </ModalTemplate>
+  </Teleport>
+</template>
+
+<style scoped></style>
