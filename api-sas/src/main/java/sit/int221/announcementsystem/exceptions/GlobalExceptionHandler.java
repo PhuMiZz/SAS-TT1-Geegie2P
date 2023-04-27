67e226ec0d791f9ead3edd30d855c97e4d1d8751
banlearit <banlearit.siri@gmail.com>
2023-04-27 17:39:07 +0700

    feat: add exceptions

---
+++ api-sas/src/main/java/sit/int221/announcementsystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package sit.int221.announcementsystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> itemNotFound(ItemNotFoundException exception, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                exception.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}
--- api-sas/src/main/java/sit/int221/announcementsystem/services/AnnouncementService.java
+++ api-sas/src/main/java/sit/int221/announcementsystem/services/AnnouncementService.java
@@ -1,10 +1,9 @@
 package sit.int221.announcementsystem.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import sit.int221.announcementsystem.entities.Announcement;
+import sit.int221.announcementsystem.exceptions.ItemNotFoundException;
 import sit.int221.announcementsystem.repositories.AnnouncementRepository;
 
 import java.util.List;
@@ -18,8 +17,8 @@ public class AnnouncementService {
     }
 
     public Announcement getAnnouncementDetail(int announcementId){
-        return repository.findById(announcementId).orElseThrow(() -> new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Announcement ID: " + announcementId + "DOES NOT EXIST!!!"
-        ));
+        return repository.findById(announcementId).orElseThrow(
+                () -> new ItemNotFoundException("Announcement ID: " + announcementId + " do not exist!.")
+        );
     }
 }
