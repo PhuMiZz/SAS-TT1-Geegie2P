7f55f8a821d941dd09c1a13210b3db5e4725f140
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2023-04-27 11:42:13 +0700

    fix announcement layout

--- front-sas/src/components/UI/molecules/SingleAnnouncement.vue
+++ front-sas/src/components/UI/molecules/SingleAnnouncement.vue
@@ -1,8 +1,8 @@
 <script setup>
-import BadgeCategories from './BadgeCategories.vue';
-import DetailIcon from '../atoms/DetailIcon.vue';
-import AnnouncementDetail from '../organisms/AnnouncementDetail.vue';
-import { onMounted, reactive, ref, watchEffect } from 'vue';
+import BadgeCategories from "./BadgeCategories.vue";
+import DetailIcon from "../atoms/DetailIcon.vue";
+import AnnouncementDetail from "../organisms/AnnouncementDetail.vue";
+import { onMounted, reactive, ref, watchEffect } from "vue";
 const props = defineProps({
   announcementItem: {
     type: Object,
@@ -11,8 +11,8 @@ const props = defineProps({
 });
 
 const badgeCategoryColor = reactive({
-  textColor: '',
-  bgColor: '',
+  textColor: "",
+  bgColor: "",
 });
 const showModal = ref(false);
 
@@ -23,23 +23,23 @@ watchEffect(() => {
   const categoryId = props.announcementItem.category.categoryId;
   switch (categoryId) {
     case 1:
-      badgeCategoryColor.bgColor = 'bg-green-300';
-      badgeCategoryColor.textColor = 'text-[#145941]';
+      badgeCategoryColor.bgColor = "bg-green-300";
+      badgeCategoryColor.textColor = "text-[#145941]";
       // console.log('ทั่วไป');
       break;
     case 2:
-      badgeCategoryColor.bgColor = 'bg-[#55CAF5]';
-      badgeCategoryColor.textColor = 'text-[#244F5F]';
+      badgeCategoryColor.bgColor = "bg-[#55CAF5]";
+      badgeCategoryColor.textColor = "text-[#244F5F]";
       // console.log('ทุนการศึกษา');
       break;
     case 3:
-      badgeCategoryColor.bgColor = 'bg-[#FF9DA8]';
-      badgeCategoryColor.textColor = 'text-[#8D212D]';
+      badgeCategoryColor.bgColor = "bg-[#FF9DA8]";
+      badgeCategoryColor.textColor = "text-[#8D212D]";
       // console.log('หางาน');
       break;
     case 4:
-      badgeCategoryColor.bgColor = 'bg-yellow-300';
-      badgeCategoryColor.textColor = 'text-yellow-800';
+      badgeCategoryColor.bgColor = "bg-yellow-300";
+      badgeCategoryColor.textColor = "text-yellow-800";
       // console.log('ฝึกงาน');
       break;
   }
@@ -48,17 +48,17 @@ watchEffect(() => {
 
 <template>
   <div
-    class="flex items-center justify-evenly bg-gray-100 w-full h-28 my-2 rounded-xl"
+    class="flex items-center justify-evenly bg-gray-100 w-full h-24 my-2 rounded-xl"
   >
     <div
       class="flex items-center justify-center w-1/5 h-full bg-[#E87B92] rounded-l-xl"
     >
-      <h1 class="text-3xl">{{ announcementItem.id }}</h1>
+      <h1 class="text-xl">{{ announcementItem.id }}</h1>
     </div>
-    <div class="text-xl w-full p-5">
+    <div class="flex w-2/3 p-5">
       {{ announcementItem.announcementTitle }}
     </div>
-    <div class="w-1/3 text-center">
+    <div class="flex w-1/3 text-center">
       <BadgeCategories
         :bg-color="badgeCategoryColor.bgColor"
         :text-color="badgeCategoryColor.textColor"
@@ -66,13 +66,15 @@ watchEffect(() => {
       >
     </div>
 
-    <div class="text-xl text-center w-2/3">
+    <div class="flex text-center w-2/3 text-[#737373]">
       {{ announcementItem.publishDate }}
     </div>
-    <div class="text-xl text-center w-2/3">
+    <div class="flex text-center w-2/3 text-[#737373]">
       {{ announcementItem.closeDate }}
     </div>
-    <div class="text-xl text-center w-1/4">{{ announcementItem.display }}</div>
+    <div class="flex text-center w-1/4 text-[#737373]">
+      {{ announcementItem.display }}
+    </div>
     <button class="flex justify-center w-1/4" @click="onClickDetail">
       <DetailIcon />
     </button>
