bd6dc301bc5af3aac07215201c82caa6fd18c633
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-04-30 20:52:24 +0700

    Update AlertOverlay.vue

--- front-sas/src/components/UI/organisms/AlertOverlay.vue
+++ front-sas/src/components/UI/organisms/AlertOverlay.vue
@@ -2,10 +2,10 @@
 
 <template>
   <div
-    class="bg-red-900 text-center py-4 lg:px-4 flex gap-x-3 justify-center rounded-full"
+    class="bg-red-900 text-center py-4 px-4 flex flex-col gap-2 mx-5 md:mx-0 md:gap-3 md:flex-row justify-center rounded-2xl md:rounded-full"
   >
     <div
-      class="p-2 bg-red-800 items-center text-slate-100 leading-none lg:rounded-full flex lg:inline-flex"
+      class="p-2 bg-red-800 items-center text-slate-100 leading-none rounded-full flex lg:inline-flex"
       role="alert"
     >
       <span
@@ -16,7 +16,9 @@
         >The requested page is not available!</span
       >
     </div>
-    <button class="text-slate-100 bg-red-800 px-3 lg:rounded-full">
+    <button
+      class="text-slate-100 bg-red-800 py-1 px-10 w-fit mx-auto md:mx-0 md:py-0 md:px-4 rounded-full"
+    >
       <router-link to="/"> OK </router-link>
     </button>
   </div>
