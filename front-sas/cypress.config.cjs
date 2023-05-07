1086d0a3714f821b47e052d7971bb33a288b0868
banlearit <banlearit.siri@gmail.com>
2023-05-07 13:16:30 +0700

    fix cypress

--- front-sas/cypress.config.cjs
+++ front-sas/cypress.config.cjs
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-   baseUrl:"http://intproj22.sit.kmutt.ac.th/tt1",
+   baseUrl:"http://localhost:5173/",
    experimentalSessionAndOrigin: true
   },
 });
