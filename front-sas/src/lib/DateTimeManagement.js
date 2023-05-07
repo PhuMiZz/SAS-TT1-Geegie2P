7da5c20607fe287c3c5593ea88d9b9436666691e
banlearit <banlearit.siri@gmail.com>
2023-05-07 08:24:09 +0700

    Fix extractDateAndTime

--- front-sas/src/lib/DateTimeManagement.js
+++ front-sas/src/lib/DateTimeManagement.js
@@ -8,7 +8,8 @@ function getLocaleDateTime(dateTime) {
     minute: '2-digit',
   };
   const userLocale = navigator.language || 'en-GB';
-  return new Intl.DateTimeFormat(userLocale, option).format(date);
+  const defaultLocale = 'en-GB';
+  return new Intl.DateTimeFormat(defaultLocale, option).format(date);
 }
 
 function getISODateTime(date, time) {
@@ -17,26 +18,46 @@ function getISODateTime(date, time) {
   return newDate.toISOString();
 }
 
+// function extractDateAndTime(dateTime) {
+//   if (!dateTime) return ['', ''];
+//
+//   const gmtString = new Date(dateTime);
+//   const date = gmtString.getDate();
+//   const month = gmtString.getMonth();
+//   const year = gmtString.getFullYear();
+//   // const time = gmtString.toLocaleTimeString().substring(0, 5);
+//   const time = gmtString.toString().substring(16, 21);
+//
+//   const fillZero = (n) => {
+//     return n < 10 ? '0' + n : n;
+//   };
+//   const dateFormat = year + '-' + fillZero(month + 1) + '-' + fillZero(date);
+//
+//   // const isoString = new Date(dateTime).toISOString();
+//   // const date = isoString.split('T')[0];
+//   // const time = isoString.split('T')[1].substring(0, 5);
+//
+//   return [dateFormat, time];
+// }
 function extractDateAndTime(dateTime) {
   if (!dateTime) return ['', ''];
 
   const gmtString = new Date(dateTime);
-  const date = gmtString.getDate();
-  const month = gmtString.getMonth();
-  const year = gmtString.getFullYear();
-  // const time = gmtString.toLocaleTimeString().substring(0, 5);
-  const time = gmtString.toString().substring(16, 21);
 
-  const fillZero = (n) => {
-    return n < 10 ? '0' + n : n;
-  };
-  const dateFormat = year + '-' + fillZero(month + 1) + '-' + fillZero(date);
+  const userLocale = navigator.language || 'en-GB';
+  const formattedDateTime = new Intl.DateTimeFormat(userLocale, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  }).format(gmtString);
 
-  // const isoString = new Date(dateTime).toISOString();
-  // const date = isoString.split('T')[0];
-  // const time = isoString.split('T')[1].substring(0, 5);
+  const date = formattedDateTime.split(',')[0].replace(/\//g, '-');
+  const time = formattedDateTime.split(',')[1].trim().substring(0, 5);
 
-  return [dateFormat, time];
+  return [date, time];
 }
 
 export { getLocaleDateTime, getISODateTime, extractDateAndTime };
