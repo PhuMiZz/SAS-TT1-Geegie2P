a536cfc9de6d961889ed50c738dabc790bf3d92d
banlearit <banlearit.siri@gmail.com>
2023-05-10 15:44:45 +0700

    Fix closeDate if null

--- api-sas/src/main/java/sit/int221/announcementsystem/repositories/AnnouncementRepository.java
+++ api-sas/src/main/java/sit/int221/announcementsystem/repositories/AnnouncementRepository.java
@@ -22,15 +22,16 @@ public interface AnnouncementRepository extends JpaRepository<Announcement,Integ
     @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId ORDER BY a.publishDate DESC, a.closeDate DESC")
     Page<Announcement> findByCategoryOrderByPublishDateDescCloseDateDesc(@Param("categoryId") Integer categoryId, Pageable pageable);
 
-    @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = 'Y' AND a.closeDate > CURRENT_TIMESTAMP")
+    @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = 'Y' AND (a.closeDate IS NULL OR a.closeDate > CURRENT_TIMESTAMP)")
     Page<Announcement> findActiveAnnouncements(Pageable pageable);
 
-    @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId AND a.announcementDisplay = 'Y' AND a.closeDate > CURRENT_TIMESTAMP")
+    @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId AND a.announcementDisplay = 'Y' AND (a.closeDate IS NULL OR a.closeDate > CURRENT_TIMESTAMP)")
     Page<Announcement> findActiveAnnouncementsByCategory(@Param("categoryId") Integer categoryId, Pageable pageable);
 
-    @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = 'Y' AND a.closeDate <= CURRENT_TIMESTAMP")
+    @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = 'Y' AND a.closeDate IS NOT NULL AND a.closeDate <= CURRENT_TIMESTAMP")
     Page<Announcement> findClosedAnnouncements(Pageable pageable);
 
-    @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId AND a.announcementDisplay = 'Y' AND a.closeDate <= CURRENT_TIMESTAMP")
+    @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId AND a.announcementDisplay = 'Y' AND a.closeDate IS NOT NULL AND a.closeDate <= CURRENT_TIMESTAMP")
     Page<Announcement> findClosedAnnouncementsByCategory(@Param("categoryId") Integer categoryId, Pageable pageable);
+
 }
