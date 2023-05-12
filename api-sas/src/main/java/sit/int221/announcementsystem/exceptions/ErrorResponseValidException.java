f19860afa83dcbaf7a0c6fa6acf621e3fe9c9868
banlearit <banlearit.siri@gmail.com>
2023-05-12 18:03:24 +0700

    Fix ValidException

---
+++ api-sas/src/main/java/sit/int221/announcementsystem/exceptions/ErrorResponseValidException.java
@@ -0,0 +1,28 @@
+package sit.int221.announcementsystem.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseValidException {
+    private int status;
+    private String title;
+    private String instance;
+    private List<FieldError> detail;
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class FieldError {
+        private String field;
+        private String errorMessage;
+
+    }
+}
