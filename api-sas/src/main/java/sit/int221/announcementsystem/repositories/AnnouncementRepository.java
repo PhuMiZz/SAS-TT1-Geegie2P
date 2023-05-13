179969ba992c00383ea74ca55cd6007abb9c3cbf
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-05-13 11:54:13 +0700

    Delete comments in AnnoucementRepository

--- api-sas/src/main/java/sit/int221/announcementsystem/repositories/AnnouncementRepository.java
+++ api-sas/src/main/java/sit/int221/announcementsystem/repositories/AnnouncementRepository.java
@@ -19,8 +19,6 @@ public interface AnnouncementRepository extends JpaRepository<Announcement,Integ
     @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = :displayStatus AND (a.closeDate IS NULL OR a.closeDate > :currentDate) AND (a.publishDate IS NULL OR a.publishDate <= :currentDate) ORDER BY a.id DESC")
     List<Announcement> findActiveAnnouncementsWithDisplayStatus(@Param("displayStatus") Announcement.DisplayStatus displayStatus, @Param("currentDate") ZonedDateTime currentDate);
 
-//    @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = :displayStatus AND (a.closeDate IS NOT NULL AND a.closeDate <= CURRENT_TIMESTAMP) ORDER BY a.publishDate DESC, a.closeDate DESC")
-//    List<Announcement> findClosedAnnouncementsWithDisplayStatus(@Param("displayStatus") Announcement.DisplayStatus displayStatus);
     @Query("SELECT a FROM Announcement a WHERE a.announcementDisplay = :displayStatus AND a.closeDate IS NOT NULL AND a.closeDate <= :currentDate ORDER BY a.id DESC ")
     List<Announcement> findClosedAnnouncementsWithDisplayStatus(@Param("displayStatus") Announcement.DisplayStatus displayStatus, @Param("currentDate") ZonedDateTime currentDate);
 
@@ -39,14 +37,6 @@ public interface AnnouncementRepository extends JpaRepository<Announcement,Integ
 
     @Query("SELECT a FROM Announcement a WHERE a.category.id = :categoryId AND a.announcementDisplay = 'Y' AND a.closeDate IS NOT NULL AND a.closeDate <= :currentDate ORDER BY a.id DESC ")
     Page<Announcement> findClosedAnnouncementsByCategory(@Param("categoryId") Integer categoryId, Pageable pageable, ZonedDateTime currentDate);
-//    Page<Announcement> findByCategoryOrderByPublishDateDescCloseDateDesc(Category category, Pageable pageable);
-//
-//    Page<Announcement> findByAnnouncementDisplayAndCloseDateNullOrCloseDateAfterOrderByPublishDateDesc(Announcement.DisplayStatus displayStatus, ZonedDateTime closeDate, Pageable pageable);
-//
-//    Page<Announcement> findByCategoryAndAnnouncementDisplayAndCloseDateIsNullOrderByPublishDateDesc(Category category, Announcement.DisplayStatus displayStatus, Pageable pageable);
-//
-//    Page<Announcement> findByAnnouncementDisplayAndCloseDateIsNotNullAndCloseDateBeforeOrderByPublishDateDesc(Announcement.DisplayStatus displayStatus, ZonedDateTime closeDate, Pageable pageable);
-//
-//    Page<Announcement> findByCategoryAndAnnouncementDisplayAndCloseDateIsNotNullAndCloseDateBeforeOrderByPublishDateDesc(Category category, Announcement.DisplayStatus displayStatus, ZonedDateTime closeDate, Pageable pageable);a
+
 
 }
