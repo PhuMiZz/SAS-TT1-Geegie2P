bd1066a92565ae1d70bd0aa730fdcdd28f708253
PhuMiZz <siripoom.kuso@kmutt.ac.th>
2023-05-16 00:43:15 +0700

    Update ExceptionHandlerValid.java

--- api-sas/src/main/java/sit/int221/announcementsystem/exceptions/ExceptionHandlerValid.java
+++ api-sas/src/main/java/sit/int221/announcementsystem/exceptions/ExceptionHandlerValid.java
@@ -3,16 +3,20 @@ package sit.int221.announcementsystem.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
+
+@ControllerAdvice
 public class ExceptionHandlerValid {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseValidException> handleMethodArgumentNotValid(org.springframework.web.bind.MethodArgumentNotValidException ex, WebRequest request) {
+    public ResponseEntity<ErrorResponseValidException> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request) {
         ErrorResponseValidException apiError = new ErrorResponseValidException();
         apiError.setStatus(HttpStatus.BAD_REQUEST.value());
         apiError.setTitle("Announcement attributes validation failed!");
